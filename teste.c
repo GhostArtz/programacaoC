3a727a4b085ff0288910ad8a08cc8cf6eeed8a48
GhostArtz <artursiqueirasevero@gmail.com>
2026-02-26 16:41:23 +0000

    gerenciamento de inventario simples

--- teste.c
+++ teste.c
@@ -1,20 +1,42 @@
 #include <stdio.h>
 
-int main(){
+int main() {
 
-    int nota1, nota2, nota3;
-    float media;
+    // Variaveis
 
-    printf("Digite a primeira nota do aluno: \n");
-    scanf("%d", &nota1);
-    printf("Digite a segunda nota do aluno: \n");
-    scanf("%d", &nota2);
-    printf("Digite a terceira nota do aluno: \n");
-    scanf("%d", &nota3);
+   char produtoA[30] = "Produto A";
+   char produtoB[30] = "Produto B";
 
-    media = (float) (nota1 + nota2 + nota3) / 3;
+   unsigned int estoqueA = 1000;
+   unsigned int estoqueB = 2000;
 
-    printf("A média do aluno no bimestre foi: %.1f\n", media);
+   float valorA = 10.50;
+   float valorB = 20.40;
 
-    return 0;
+   unsigned int estoqueMinimoA = 500;
+   unsigned int estoqueMinimoB = 2500;
+
+   double valorTotalA = (valorA * estoqueA);
+   double valorTotalB = (valorB * estoqueB);
+
+   int resultadoA, resultadoB;
+
+   // Informações dos produtos
+
+   printf("O %s tem estoque %u e valor unitário R$ %.2f\n", produtoA, estoqueA, valorA);
+   printf("O %s tem estoque %u e valor unitário R$ %.2f\n", produtoB, estoqueB, valorB);
+
+   // Comparação do valor minimo de estoques
+    resultadoA = estoqueA > estoqueMinimoA;
+    resultadoB = estoqueB > estoqueMinimoB;
+
+    printf("O %s tem estoque minimo? %u\n", produtoA, resultadoA);
+    printf("O %s tem estoque minimo? %u\n", produtoB, resultadoB);
+
+    // Comparação entre os valores totais dos produtos
+
+    printf("O valor total de A(R$ %.2f) é maior que o valor total de B(R$ %.2f)? %d\n", valorTotalA, valorTotalB, (valorTotalA) > (valorTotalB));
+
+   return 0;
+
 }
